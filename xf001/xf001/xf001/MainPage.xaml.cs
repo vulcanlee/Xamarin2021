1455347497e81a4666deefb09eed2c87cb72b843
Vulcan <vulcan.lee@gmail.com>
2021-09-24 22:29:02 +0800

    xf001|設計第一個 Hello World 應用程式 – 使用 Code-behind

---
+++ xf001/xf001/xf001/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xf001
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BtnHello.Clicked += (s, e) =>
+            {
+                LabelName.Text = EntryName.Text;
+                Console.WriteLine($"{EntryName.Text}");
+            };
+        }
+    }
+}
